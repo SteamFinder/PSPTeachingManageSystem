bb73cc017bf53a2240964a8e2f4fe55b22a5f131
thetheorange <8106220519@csu.edu.cn>
2023-05-13 21:11:18 +0800

    修复乱码问题

--- SQL连接/test.py
+++ SQL连接/test.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    with open("student_profile.html") as f:
+    with open("student_profile.html", encoding="gb2312") as f:
         content = f.read()
     return content
 
