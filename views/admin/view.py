32f947bba5754bc431d42c7eaaa7be03bc9fc2e4
thetheorange <8106220519@csu.edu.cn>
2023-05-15 22:19:48 +0800

    1.完善MVT构架 2.实现基本查询工作（有bug）

--- views/admin/view.py
+++ views/admin/view.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from sqlalchemy.orm import sessionmaker
 
 from models.models import engine, St_Info, S_C_Info
@@ -13,3 +13,31 @@ def index():
     scores = session.query(S_C_Info).all()
     session.close()
     return render_template("student_profile.html", users=students, scores=scores)
+
+
+@admin_blu.route("/select_student")
+def select_student():
+    ret = request.args
+    if ret:
+        user_name = request.args.get("username")
+        DBSession = sessionmaker(bind=engine)
+        session = DBSession()
+        result = session.query(St_Info).filter(St_Info.St_Name == user_name).all()
+        session.close()
+        return render_template("student_profile.html", users=result)
+    else:
+        return redirect(url_for("admin.index"))
+
+
+@admin_blu.route("/select_score")
+def select_score():
+    ret = request.args
+    if ret:
+        userid_score = request.args.get("score")
+        DBSession = sessionmaker(bind=engine)
+        session = DBSession()
+        result = session.query(S_C_Info).filter(S_C_Info.St_ID == userid_score).all()
+        session.close()
+        return render_template("student_profile.html", scores=result)
+    else:
+        return redirect(url_for("admin.index"))
