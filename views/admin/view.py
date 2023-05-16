057899c7fdce74e13f3e4b1e33f8819149e1786e
thetheorange <8106220519@csu.edu.cn>
2023-05-16 18:21:27 +0800

    1.实现删除功能 2.增加刷新功能，用以解决查询界面空白问题

--- views/admin/view.py
+++ views/admin/view.py
@@ -6,6 +6,7 @@ from . import admin_blu
 
 
 @admin_blu.route("/")
+# 主页面
 def index():
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
@@ -15,7 +16,8 @@ def index():
     return render_template("student_profile.html", users=students, scores=scores)
 
 
-@admin_blu.route("/select_student")
+@admin_blu.route("/select_student", methods=["get", "post"])
+# 查询学生信息
 def select_student():
     ret = request.args
     if ret:
@@ -26,18 +28,39 @@ def select_student():
         session.close()
         return render_template("student_profile.html", users=result)
     else:
-        return redirect(url_for("admin.index"))
+        pass
 
 
-@admin_blu.route("/select_score")
+@admin_blu.route("/delete_student")
+# 删除学生信息数据
+def delete_student():
+    ret = request.args
+    if ret:
+        user_name = request.args.get("username")
+        DBSession = sessionmaker(bind=engine)
+        session = DBSession()
+        session.query(St_Info).filter(St_Info.St_Name == user_name).delete()
+        session.commit()
+        session.close()
+        return render_template("student_profile.html")
+    else:
+        pass
+
+
+@admin_blu.route("/select_score", methods=["get", "post"])
+# 查询学生成绩
 def select_score():
     ret = request.args
     if ret:
-        userid_score = request.args.get("score")
+        userid_score = request.args.get("c_num")
         DBSession = sessionmaker(bind=engine)
         session = DBSession()
-        result = session.query(S_C_Info).filter(S_C_Info.St_ID == userid_score).all()
+        result = session.query(S_C_Info).filter(S_C_Info.C_No == userid_score).all()
         session.close()
         return render_template("student_profile.html", scores=result)
     else:
-        return redirect(url_for("admin.index"))
+        pass
+
+
+
+
