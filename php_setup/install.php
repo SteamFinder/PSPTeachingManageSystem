11503a9defebaf59561f3e53bedf144475c7531c
SteamFinder <steamfinder@foxmail.com>
2023-05-23 19:28:56 +0800

    输出配置

--- php_setup/install.php
+++ php_setup/install.php
@@ -9,14 +9,14 @@
  * DBAdmin      数据库用户名
  * DBPassword   数据库密码
 */
-$PHPServerIP = $_GET["PHPServerIP"] ?? NULL;
-$PyServerIP = $_GET["PyServerIP"] ?? NULL;
+$PHPServerIP = $_POST["PHPServerIP"] ?? NULL;
+$PyServerIP = $_POST["PyServerIP"] ?? NULL;
 $DBType = "Microsoft SQL Server";//锁定
 $DBVer = "2019";//锁定
-$DBIP = $_GET["DBIP"] ?? NULL;
-$DBPort = $_GET["DBPort"] ?? NULL;
-$DBAdmin = $_GET["DBAdmin"] ?? NULL;
-$DBPassword = $_GET["DBPassword"] ?? NULL;
+$DBIP = $_POST["DBIP"] ?? NULL;
+$DBPort = $_POST["DBPort"] ?? NULL;
+$DBAdmin = $_POST["DBAdmin"] ?? NULL;
+$DBPassword = $_POST["DBPassword"] ?? NULL;
     if($PHPServerIP == NULL || $PyServerIP == NULL
         || $DBIP == NULL    || $DBPort == NULL
         ||$DBAdmin == NULL  || $DBPassword == NULL){
@@ -32,11 +32,25 @@ $DBPassword = $_GET["DBPassword"] ?? NULL;
         'DBAdmin' => $DBAdmin,
         'DBPassword' => $DBPassword,
     );
-    echo json_encode($SetupInfo);
+    //写入php_setup
     //w:只写。打开并清空文件的内容；如果文件不存在，则创建新文件。
     $OutConfig = fopen("config.json","w");
     //fwrite 写入json
     fwrite($OutConfig, json_encode($SetupInfo));
     //关闭连接
     fclose($OutConfig);
-//die("<meta http-equiv=\"refresh\" content=\"0;url=/public/index.php?info=安装成功\">");
+    //写入根目录
+    //w:只写。打开并清空文件的内容；如果文件不存在，则创建新文件。
+    $OutConfig = fopen("../config.json","w");
+    //fwrite 写入json
+    fwrite($OutConfig, json_encode($SetupInfo));
+    //关闭连接
+    fclose($OutConfig);
+    //写入php_service
+    //w:只写。打开并清空文件的内容；如果文件不存在，则创建新文件。
+    $OutConfig = fopen("../php_service/config.json","w");
+    //fwrite 写入json
+    fwrite($OutConfig, json_encode($SetupInfo));
+    //关闭连接
+    fclose($OutConfig);
+    die("<meta http-equiv=\"refresh\" content=\"0;url=../setup.php?info=安装成功&status=success\">");
