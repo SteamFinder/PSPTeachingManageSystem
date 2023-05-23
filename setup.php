e051b03ab97a4c74efd7aea95c2f2e9531864996
SteamFinder <steamfinder@foxmail.com>
2023-05-23 19:29:30 +0800

    完善安装功能

--- setup.php
+++ setup.php
@@ -10,6 +10,7 @@
  * DBPassword   数据库密码
 */
 $info = $_GET["info"] ?? NULL;
+$status = $_GET["status"] ?? NULL;
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,6 +24,28 @@ $info = $_GET["info"] ?? NULL;
     <!-- 最新的 Bootstrap5 核心 JavaScript 文件 -->
     <script src="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/js/bootstrap.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--    <style>-->
+<!--        /*运动渐变色*/-->
+<!--        body{-->
+<!--            color: #fff;-->
+<!--            background-image: linear-gradient(-->
+<!--            125deg,#2c3e50,#27ae60, #54b0ef, #0077ff,#53e3fb-->
+<!--            );-->
+<!--            background-size: 400%;-->
+<!--            animation: bgmove 20s infinite;-->
+<!--        }-->
+<!--        @keyframes bgmove {-->
+<!--            0%{-->
+<!--                background-position: 0  50%;-->
+<!--            }-->
+<!--            50%{-->
+<!--                background-position: 100%  50%;-->
+<!--            }-->
+<!--            100%{-->
+<!--                background-position: 0  50%;-->
+<!--            }-->
+<!--        }-->
+<!--    </style>-->
 </head>
 <body>
     <div class="container">
@@ -42,14 +65,23 @@ $info = $_GET["info"] ?? NULL;
         <div class="row">
             <div class="col-3"></div>
             <div class="col-6">
-                <form action="./sys_setup/install.php">
+                <form action="./php_setup/install.php" method="post">
                     <div class="row">
                         <?php
                         if($info != NULL){
-                        echo "<div class=\"alert alert-warning\">";
-                        echo "<strong>注意!</strong>" . $info;
-                        echo "</div>";
-
+                            if($status == 'success'){
+                                echo "<div class=\"alert alert-success\">";
+                                echo "<strong>注意! </strong>" . $info;
+                                echo "</div>";
+                            }else if($status == 'wrong'){
+                                echo "<div class=\"alert alert-warning\">";
+                                echo "<strong>注意! </strong>" . $info;
+                                echo "</div>";
+                            }else{
+                                echo "<div class=\"alert alert-dark\">";
+                                echo "<strong>注意! </strong> 提示信息无法正确显示<br>返回值参数出现错误(GET)";
+                                echo "</div>";
+                            }
                         }
                         ?>
                         <div class="alert alert-info">
@@ -113,7 +145,9 @@ $info = $_GET["info"] ?? NULL;
                         </div>
                     </div>
                     <br>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <div class="row">
+                        <button type="submit" class="btn btn-primary">提交设置</button>
+                    </div>
                 </form>
             </div>
             <div class="col-3"></div>
